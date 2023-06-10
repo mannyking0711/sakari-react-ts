00e8e06d5220cb4153d09936d11091e6ec7dc707
mannyking0711 <onegoal711@gmail.com>
2023-06-09 20:32:59 -0400

    Sakari prime react theme

--- sakai-ts/pages/auth/login/index.tsx
+++ sakai-ts/pages/auth/login/index.tsx
@@ -26,7 +26,7 @@ const LoginPage: Page = () => {
                 <div style={{ borderRadius: '56px', padding: '0.3rem', background: 'linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)' }}>
                     <div className="w-full surface-card py-8 px-5 sm:px-8" style={{ borderRadius: '53px' }}>
                         <div className="text-center mb-5">
-                            <img src="/demo/images/login/avatar.png" alt="Image" height="50" className="mb-3" />
+                            <img src="/demo/images/login/avatar.png" alt="Image" height="56" className="mb-3" />
                             <div className="text-900 text-3xl font-medium mb-3">Welcome, Isabel!</div>
                             <span className="text-600 font-medium">Sign in to continue</span>
                         </div>
